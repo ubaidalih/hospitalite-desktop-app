e3ae20a176d65058250a2c7be0a8d5aff6419726
Ubaidillah Ariq <ubaidillahariq.rea@ruangguru.com>
2022-04-11 22:39:39 +0700

    feat : add register logic

---
+++ backend/register.py
@@ -0,0 +1,26 @@
+import psycopg2 as pg
+
+def register(registerInfo,isDokter, conn, com):
+    if isDokter :
+        com.execute("SELECT * FROM dokter")
+        listAkun = com.fetchall()
+    else :
+        com.execute("SELECT * FROM pasien")
+        listAkun = com.fetchall()
+    
+    for i in range (len(listAkun)):
+        if registerInfo[1] == listAkun[i][2]:
+            print("Email sudah ada")
+            return False
+        elif registerInfo[2] == listAkun[i][3]:
+            print("Username sudah ada")
+            return False
+
+    idAkun = len(listAkun) + 1
+    if isDokter :
+        com.execute("INSERT INTO dokter VALUES ({}, '{}', '{}', '{}', '{}')".format(idAkun, registerInfo[0], registerInfo[1], registerInfo[2], registerInfo[3]))
+    else :
+        com.execute("INSERT INTO pasien VALUES ({}, '{}', '{}' ,'{}' ,'{}')".format(idAkun, registerInfo[0], registerInfo[1], registerInfo[2], registerInfo[3]))
+    conn.commit()
+    print("Data berhasil ditambahkan")
+    return True
