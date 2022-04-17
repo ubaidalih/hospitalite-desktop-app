8fc66dc625963c3c143eda248b55df31d9d8bb42
Ubaidillah Ariq <ubaidillahariq.rea@ruangguru.com>
2022-04-17 15:11:42 +0700

    fix pylint lagi

--- src/backend/register.py
+++ src/backend/register.py
@@ -1,14 +1,14 @@
 import psycopg2 as pg
 
-def register(registerInfo,isDokter, conn, com):
-    if isDokter :
+def register(registerInfo, isDokter, conn, com):
+    if isDokter:
         com.execute("SELECT * FROM dokter")
         listAkun = com.fetchall()
-    else :
+    else:
         com.execute("SELECT * FROM pasien")
         listAkun = com.fetchall()
-    
-    for i in range (len(listAkun)):
+
+    for i in range(len(listAkun)):
         if registerInfo[1] == listAkun[i][2]:
             print("Email sudah ada")
             return False
@@ -17,9 +17,9 @@ def register(registerInfo,isDokter, conn, com):
             return False
 
     idAkun = len(listAkun) + 1
-    if isDokter :
+    if isDokter:
         com.execute("INSERT INTO dokter VALUES ({}, '{}', '{}', '{}', '{}')".format(idAkun, registerInfo[0], registerInfo[1], registerInfo[2], registerInfo[3]))
-    else :
+    else:
         com.execute("INSERT INTO pasien VALUES ({}, '{}', '{}' ,'{}' ,'{}')".format(idAkun, registerInfo[0], registerInfo[1], registerInfo[2], registerInfo[3]))
     conn.commit()
     print("Data berhasil ditambahkan")
